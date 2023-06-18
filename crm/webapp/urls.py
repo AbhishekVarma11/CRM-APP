6898951447101679d1d4270ad16c62d2afff26d4
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-06-18 11:19:11 +0530

    add some files like record,registration page etc

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -5,6 +5,7 @@ from django.urls import path,include
 urlpatterns = [
     path('',views.home,name='home'),
     path('logout/',views.logout_user,name='logout'),
-     path('register/',views.register_user,name='register'),
+    path('register/',views.register_user,name='register'),
+    path('record/<int:pk>',views.customer_record,name='record')
    
 ]
--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 # Create your views here.
 def home(request):
+    records=Record.objects.all()
     if request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
@@ -15,7 +18,7 @@ def home(request):
             messages.success(request,"There was an error logging!")
             return redirect('home')
     else:
-        return render(request,'webapp/home.html',{})
+        return render(request,'webapp/home.html',{'records':records})
 
 
 def logout_user(request):
@@ -24,6 +27,26 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request,'webapp/register.html',{})
-
-
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,"You have been logged in!")
+            return redirect('home')
+    else:
+        form=SignUpForm()
+        return render(request,'webapp/register.html',{'form':form})
+    
+    return render(request,'webapp/register.html',{'form':form})
+    
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record=Record.objects.get(id=pk)
+        return render(request,'webapp/record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request,"you must be logged in to view the records")
+        return redirect('home')
